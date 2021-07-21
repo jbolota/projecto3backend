518da4a4795c0bdd14d2cba3603250e49df60297
jbolota <franciscocarvalho88@gmail.com>
2021-07-21 15:34:13 +0100

    Update database.js

--- src/model/database.js
+++ src/model/database.js
@@ -1,13 +1,18 @@
 var Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    'projetofinal',
-    'postgres',
-    '123456',
+    'd7vs6j2c75jq23',
+    'keghkvjtnpouuo',
+    'c7c701f9997743c4bb9b08f3cedd83c3c87c5e32c49dd527e8c94d2a6be7ec66',
     {
-        host: 'localhost',
+        host: 'ec2-35-174-56-18.compute-1.amazonaws.com',
         port: '5432',
-        dialect: 'postgres'
+        dialect: 'postgres',
+        dialectOptions:{
+            ssl:{
+                rejectUnauthorized: false,
+            }
+        }
     },
 
 );
